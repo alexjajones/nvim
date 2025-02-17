e4e25b744708a98fbded0c3610dff5ee31551a2a
Alex Jones <alexjajones1@gmail.com>
2025-02-17 21:05:24 +0000

    Added typo checker on ensures

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -68,7 +68,8 @@ return {
 
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
-      'stylua', -- Used to format Lua code
+      'stylua',
+      'typos-lsp',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
