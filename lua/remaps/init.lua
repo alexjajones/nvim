a0c99ff71f44001b39f8ed69274ef0b418c99c70
Alex Jones <alexjajones1@gmail.com>
2025-02-17 20:40:38 +0000

    Better spacing

--- lua/remaps/init.lua
+++ lua/remaps/init.lua
@@ -4,7 +4,11 @@ local function applyRemaps()
   vim.g.maplocalleader = ' '
 
   vim.g.have_nerd_font = false
+
   vim.opt.tabstop = 4
+  vim.opt.shiftwidth = 4
+  vim.opt.softtabstop = 4
+  vim.opt.expandtab = true
   vim.opt.number = true
 
   vim.opt.relativenumber = true
